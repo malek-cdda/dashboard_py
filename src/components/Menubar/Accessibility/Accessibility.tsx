135eaa2b450e16c0d4c3c88a984e42022155cdef
abdul malek <143941431+malek-cdda@users.noreply.github.com>
2023-12-27 15:40:28 +0600

    zoom_in_zoom_out

--- src/components/Menubar/Accessibility/Accessibility.tsx
+++ src/components/Menubar/Accessibility/Accessibility.tsx
@@ -17,12 +17,15 @@ const data = [
   },
   {
     name: "Font Size",
+    type: "zoom_font",
   },
   {
     name: "Page Zoom",
+    type: "zoom_font",
   },
   {
     name: "Magnify",
+    type: "magnify",
   },
 ];
 const availAbleItem = [
@@ -87,24 +90,44 @@ const AccessibilityData = ({ data }: any) => {
   return (
     <div className="divide-y divide-dashed ">
       {data.map((item: any, index: any) => (
-        <div key={index} className="flex justify-between py-2">
-          <div>
-            <span></span>
-          </div>
-          <div className="p-1 flex  items-center bg-[#333333] space-x-1 rounded-full">
-            <button
-              className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full    "
-              onClick={() => handleClick("inc")}>
-              {" "}
-              <PlusIcon />{" "}
-            </button>
-            <span className="text-sm leading-[18px] font-medium">100%</span>
-            <button
-              className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full   "
-              onClick={() => handleClick("i")}>
-              <MinusIcon />
-            </button>
-          </div>
+        <div key={index} className="flex justify-between py-2 px-5">
+          {item.type == "magnify" && (
+            <>
+              <div className="flex space-x-1">
+                <span>
+                  <ZoomIcon />
+                </span>
+                <span>{item.name}</span>
+              </div>
+              <button className="p-1 flex  items-center bg-[#333333] space-x-1 rounded-full">
+                Magnify
+              </button>
+            </>
+          )}
+          {item.type == "zoom_font" && (
+            <>
+              <div className="flex space-x-1">
+                <span>
+                  <ZoomIcon />
+                </span>
+                <span>{item.name}</span>
+              </div>
+              <div className="p-1 flex  items-center bg-[#333333] space-x-1 rounded-full">
+                <button
+                  className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full    "
+                  onClick={() => handleClick("inc")}>
+                  {" "}
+                  <PlusIcon />{" "}
+                </button>
+                <span className="text-sm leading-[18px] font-medium">100%</span>
+                <button
+                  className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full   "
+                  onClick={() => handleClick("i")}>
+                  <MinusIcon />
+                </button>
+              </div>
+            </>
+          )}
         </div>
       ))}
     </div>
@@ -145,6 +168,42 @@ const MinusIcon = () => {
       fill="none">
       <path
         d="M3 6H9"
+        stroke="#F2F2F2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+const ZoomIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none">
+      <path
+        d="M9.20001 11.7H14.2"
+        stroke="#F2F2F2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.7 14.2V9.19995"
+        stroke="#F2F2F2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+        stroke="#F2F2F2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M22 22L20 20"
         stroke="#F2F2F2"
         stroke-linecap="round"
         stroke-linejoin="round"
