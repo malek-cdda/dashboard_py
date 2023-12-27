8e37a8803cafae9728d8c1719ea2c07534d64600
abdul malek <143941431+malek-cdda@users.noreply.github.com>
2023-12-27 18:17:03 +0600

    update accessability

--- src/components/Menubar/Accessibility/Accessibility.tsx
+++ src/components/Menubar/Accessibility/Accessibility.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-
+import style from "./style.module.css";
+console.log(style);
 const data = [
   {
     name: "Color Blind",
+    type: "checkbox",
   },
   {
     name: "Color",
@@ -11,9 +13,11 @@ const data = [
 
   {
     name: "Vision impaired (microphone)",
+    type: "checkbox",
   },
   {
     name: "Motor disable (Keyboard navigation)",
+    type: "checkbox",
   },
   {
     name: "Font Size",
@@ -25,15 +29,10 @@ const data = [
   },
   {
     name: "Magnify",
-    type: "magnify",
+    type: "checkbox",
   },
 ];
-const availAbleItem = [
-  { name: "Available", color: "#fff" },
-  { name: "Busy", color: "#D92D20" },
-  { name: "Do not disturb", color: "#DC6803" },
-  { name: "Appear offline", color: "#808080" },
-];
+
 const Accessibility = ({ sidebarMenuIcon, handleClick }: any) => {
   return (
     <div className=" relative">
@@ -53,7 +52,7 @@ const Accessibility = ({ sidebarMenuIcon, handleClick }: any) => {
           sidebarMenuIcon?.accessblity
             ? "block   min-h-min  borderss"
             : " h-0 text-transparent  overflow-hidden  "
-        } bg-[#1A1A1A] absolute -right-6 top-[54px] rounded-md duration-300 ease-in-out transition-all  w-80  divide-y `}>
+        } bg-[#1A1A1A] absolute -right-6 top-[54px] rounded-md duration-300 ease-in-out transition-all  w-[360px]  divide-y `}>
         <div className="flex space-x-2 py-3  px-5 ">
           <div className="flex flex-col">
             <span className="text-lg leading-[23px] font-semibold">
@@ -80,60 +79,68 @@ const AccessibilityData = ({ data }: any) => {
     const bodyZoom: any = document.body.style;
     if (value == "inc") {
       setCount(count + 5);
-      bodyZoom.zoom = `${count}%`;
+      bodyZoom.zoom = `${count + 5}%`;
       return;
     } else {
       setCount(count - 5);
-      bodyZoom.zoom = `${count}%`;
+      bodyZoom.zoom = `${count - 5}%`;
     }
   };
   return (
     <div className="divide-y divide-dashed ">
       {data.map((item: any, index: any) => (
-        <div key={index} className="flex justify-between py-2 px-5">
-          {item.type == "magnify" && (
-            <>
-              <div className="flex space-x-1">
-                <span>
-                  <ZoomIcon />
-                </span>
-                <span>{item.name}</span>
-              </div>
-              <button className="p-1 flex  items-center bg-[#333333] space-x-1 rounded-full">
-                Magnify
-              </button>
-            </>
-          )}
-          {item.type == "zoom_font" && (
-            <>
-              <div className="flex space-x-1">
-                <span>
-                  <ZoomIcon />
-                </span>
-                <span>{item.name}</span>
-              </div>
-              <div className="p-1 flex  items-center bg-[#333333] space-x-1 rounded-full">
-                <button
-                  className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full    "
-                  onClick={() => handleClick("inc")}>
-                  {" "}
-                  <PlusIcon />{" "}
-                </button>
-                <span className="text-sm leading-[18px] font-medium">100%</span>
-                <button
-                  className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full   "
-                  onClick={() => handleClick("i")}>
-                  <MinusIcon />
-                </button>
-              </div>
-            </>
-          )}
-        </div>
+        <AccessibilityMenutItem
+          item={item}
+          key={index}
+          handleClick={handleClick}
+        />
       ))}
     </div>
   );
 };
-
+const AccessibilityMenutItem = ({ item, handleClick }: any) => {
+  return (
+    <div className="flex justify-between py-3 px-5">
+      <div className="flex space-x-1">
+        <span>
+          <ZoomIcon />
+        </span>
+        <span>{item.name}</span>
+      </div>
+      <div>
+        {item.type == "zoom_font" && (
+          <div>
+            <div className="p-1 flex  items-center bg-[#333333] space-x-1 rounded-full ">
+              <button
+                className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full  hover:bg-gray-400  "
+                onClick={() => handleClick("inc")}>
+                {" "}
+                <PlusIcon />{" "}
+              </button>
+              <span className="text-sm leading-[18px] font-medium">100%</span>
+              <button
+                className="w-5 h-5 bg-[#4D4D4D] flex items-center justify-center rounded-full hover:bg-gray-400  "
+                onClick={() => handleClick("i")}>
+                <MinusIcon />
+              </button>
+            </div>
+            {item.name == "Font Size" && (
+              <span className="text-xs leading-3 font-normal text-[#B3B3B3] underline tracking-tighter mt-0 block">
+                Change font style
+              </span>
+            )}
+          </div>
+        )}
+        {item?.type == "checkbox" && (
+          <label className={style.switch}>
+            <input type="checkbox" />
+            <span className={`${style.slider} ${style.round}`}> </span>
+          </label>
+        )}
+      </div>
+    </div>
+  );
+};
 const PlusIcon = () => {
   return (
     <svg
